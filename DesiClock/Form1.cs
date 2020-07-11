298250a46b6b808affc003891665673ba2455861
Palash Bansal <palash.bansal20@gmail.com>
2020-07-11 17:56:00 +0530

    lot of changes

--- DesiClock/Form1.cs
+++ DesiClock/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.IO;
 
 namespace DesiClock
 {
@@ -17,15 +18,41 @@ namespace DesiClock
         public Form1()
         {
             InitializeComponent();
+            ///Initialize form properties
             this.FormBorderStyle = FormBorderStyle.None;
             this.Icon = Properties.Resources.ico;
             this.StartPosition = FormStartPosition.WindowsDefaultBounds;
 
-            //MessageBox.Show("wid:" + Width + "hei:" + Height); initial is 800,450- typical horizontal rectangle
+            ///initialize all FIX parameters to default values
+
+
+            ///initialize all CUSTOMIZABLE parameters to default values
+            int FormWidth = 500;
+            int FormHeight = 500;
+
+            ///create config.ini in same directory if doesn't exist
+            string currentDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+            string configFilename = "config.ini";
+            string configFilenameFull = currentDirectory + "\\" + configFilename;
+            if(File.Exists(configFilenameFull))
+            {
+                ///read file contents and update CUSTOMIZABLE parameters
+
+            }
+            else
+            {
+                ///create file with all CUSTOMIZABLE parameters with default values
+                File.Create(configFilenameFull);
+                //fill file
+            }
+
+            MessageBox.Show(configFilenameFull);
+
+            MessageBox.Show("wid:" + Width + "hei:" + Height); //initial is 800,450- typical horizontal rectangle
             //form size to be read by .ini in future version
-            Width = 500;
-            Height = 500;
-            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, Width, Height));
+            Width = FormWidth;
+            Height = FormHeight;
+            //Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, Width, Height));
             
             //this.BackColor=Color.FromArgb(50,70,100); //30,50,80 is camouflaging; to be read from .ini
             
@@ -35,7 +62,7 @@ namespace DesiClock
             pboxMainImage.Width = Width;
             pboxMainImage.Height = Height;
             pboxMainImage.Image = (Image)mainImage;
-            //Controls.Add(pboxMainImage);
+            Controls.Add(pboxMainImage);
 
             //bottom layer image, custom image by user
             Image backImage = Properties.Resources.backImage;
@@ -73,7 +100,7 @@ namespace DesiClock
             };
         }*/
 
-        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        /*[DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
         private static extern IntPtr CreateRoundRectRgn
        (
            int nLeftRect,     // x-coordinate of upper-left corner
@@ -82,7 +109,7 @@ namespace DesiClock
            int nBottomRect,   // y-coordinate of lower-right corner
            int nWidthEllipse, // width of ellipse
            int nHeightEllipse // height of ellipse
-       );
+       );*/
 
 
 
--- DesiClock/Properties/Resources.Designer.cs
+++ DesiClock/Properties/Resources.Designer.cs
@@ -61,26 +61,6 @@ namespace DesiClock.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap backImage {
-            get {
-                object obj = ResourceManager.GetObject("backImage", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap DefaultImageBg {
-            get {
-                object obj = ResourceManager.GetObject("DefaultImageBg", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon ico {
